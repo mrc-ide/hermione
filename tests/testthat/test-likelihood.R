f7127fea8fbe521473ade3baf5cc80091693fac1
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2020-07-12 17:13:06 +0100

    One more test

--- tests/testthat/test-likelihood.R
+++ tests/testthat/test-likelihood.R
@@ -26,3 +26,22 @@ test_that("probability as convolution of infectious period and incubation period
   expect_equal(log_p1, right)
 
 })
+
+
+test_that("probability with pre-symptomatic infectiousness works", {
+
+  p <- probability_offset(
+    2, 2, "dexp", "dexp", list(rate = 1), list(rate = 1)
+  )
+  ## Working out the math leads to:
+  ## h(t) = integral from -t to offset with respect to s of
+  ## exp(-(t + offset)) which is equal to
+  ## (t + offset) * exp(-(t + offset)) which is the exponetial
+  ## distribution expression with rate = t + offset and x = 1
+  right <- dexp(1, rate = 4)
+  right <- round(log(p), 4)
+
+  log_p <- round(log(p), 4)
+  expect_equal(log_p, right)
+
+})
