60dbbc78bdabaa7db3e75a737964bc5243f7092a
sangeeta <sangeetabhatia03@gmail.com>
2020-07-23 13:32:36 +0100

    Modified beta density and distribution functions

--- R/utils.R
+++ R/utils.R
@@ -29,3 +29,35 @@ beta_shape1shape22muvar <- function(shape1, shape2) {
   sigma2 <- (shape1 * shape2) / ((shape1 + shape2)^2 * (shape1 + shape2 + 1))
   list(mu = mu, sigma2 = sigma2)
 }
+
+##' Modified beta probability density function
+##'
+##' This function returns the probability density of the random
+##' variable x/xmax where x/xmax is distributed according to a beta
+##' distribution.
+##' @title Modified beta distribution
+##' @param x numeric value for which density is required
+##' @param shape1 non-negative parameter of the Beta distribution
+##' @param shape2 non-negative parameter of the Beta distribution
+##' @param xmax Maximum possible value of x
+##' @param ncp non-centrality parameter.
+##' @param log logical; if TRUE, probabilities p are given as log(p)
+##' @return modified density
+##' @author Sangeeta Bhatia
+##' @export
+dmbeta <- function(x, shape1, shape2, xmax, ncp = 0, log = FALSE) {
+
+  dbeta(x = x/xmax, shape1 = shape1, shape2 = shape2, log = log)
+
+}
+
+##' Modififed beta cumulative probability distribution function
+##' @inheritParams dmbeta
+##' @return modified distribution
+##' @author Sangeeta Bhatia
+##' @export
+pmbeta <- function(x, shape1, shape2, xmax, ncp = 0, log = FALSE) {
+
+  pbeta(x = x/xmax, shape1 = shape1, shape2 = shape2, log = log)
+
+}
